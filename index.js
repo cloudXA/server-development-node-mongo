68607bd5159c8b77f0fc1248a461f11a65116775
cloudXA <zming794@gmail.com>
2020-06-14 21:28:51 +0800

    使用promise解决回调地狱问题，对文档的操作是异步进行（交给node环境处理）,在回调函数中处理完成后的任务。所以可以使用then代替callback，前提是需要使用return 返回一个promise

--- index.js
+++ index.js
@@ -5,31 +5,35 @@ const dboper = require('./operation');
 const url = 'mongodb://127.0.0.1:27017/ele';
 const dbname = 'newDatabase'        //默认建立数据库/或者连接数据库
 
-MongoClient.connect(url, (err, client) => {
-  assert.equal(err, null);
 
-  console.log('connected correctly to server');
+MongoClient.connect(url).then((client) => {
+  console.log('Connected correctly to server');
+  const db = client.db(dbname);
 
-  const db = client.db(dbname);  //数据库名称
-  const collection = db.collection('teas'); //数据库下的文档集合，默认连接该文档或者建立文档
-  dboper.insertDocument(db, {name: '张祥', description: 'Test'}, 'author', (result) => {
-    console.log('insert Document;\n', result.ops);
+  dboper.insertDocument(db, { name: '张祥', description: 'test'}, 'author')
+    .then((result) => {
+      console.log('insert Document;\n', result.ops);
 
-    dboper.findDocuments(db, 'author', (docs) => {
+      return dboper.findDocuments(db, 'author')
+    })
+    .then((docs) => {
       console.log('found Document:\n', docs);
 
-      dboper.updateDocument(db, { name: '张祥'}, { name: '张灿', description: 'no test'}, 'author', (result) => { //将张祥对应的文本替换为张灿
-        console.log('update document:\n', result.result);
-
-        dboper.findDocuments(db, 'author', (docs) => {
-          console.log('found updated document:\n', docs);
-
-          db.dropCollection('author', (result) => {
-            console.log('dropped collection:', result);
-            client.close();
-          })
-        })
-      })
+      return dboper.updateDocument(db, { name: '张祥'}, { name: '张灿', description: 'no test'}, 'author')
     })
-  })
+    .then((result) => {
+      console.log('update document:\n', result.result);
+
+      return dboper.findDocuments(db, 'author')
+    })
+    .then((docs) => {
+      console.log('found updated document:\n', docs);
+
+      return db.dropCollection('author')
+    })
+    .then((result) => {
+      console.log('dropped collection:', result);
+      client.close();
+    })
+
 })
